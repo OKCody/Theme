6eb8f3dd5e32c862e6f7267d0ff3c2c58fb70f67
Cody Taylor <codyalantaylor@gmail.com>
2019-02-06 10:49:15 -0600

    strip newlines

--- assets/js/index.js
+++ assets/js/index.js
@@ -5,6 +5,6 @@ layout: null
 {% for page in site.pages %}
 var documents = [{
   "name": "{{page.name}}",
-  "text": "{{ page.content | markdownify | strip_html | strip }}"
+  "text": "{{ page.content | markdownify | newline_to_br | strip_newlines | replace: '<br />', ' ' | strip_html | strip }}"
 }{% unless forloop.last %},{% endunless %}]
 {% endfor %}
