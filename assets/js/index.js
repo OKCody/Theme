ec137b5a9949d363b034380a3d788f69bbd1fa7e
Cody Taylor <codyalantaylor@gmail.com>
2019-02-06 11:06:25 -0600

    loop correction

--- assets/js/index.js
+++ assets/js/index.js
@@ -6,5 +6,6 @@ var documents = [
 {
   "name": "{{page.name}}",
   "text": "{{ page.content | markdownify | newline_to_br | strip_newlines | replace: '<br />', ' ' | strip_html | strip }}"
-}{% unless forloop.last %},{% endunless %}]
+}{% unless forloop.last %},{% endunless %}
 {% endfor %}
+]
