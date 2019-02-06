b18fe6d2bdfce9758b415412b0973bae40d56a4d
Cody Taylor <codyalantaylor@gmail.com>
2019-02-06 11:00:58 -0600

    only include html_pages in index

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,9 +1,9 @@
 ---
 layout: null
 ---
-
-{% for page in site.pages %}
-var documents = [{
+var documents = [
+{% for page in site.html_pages %}
+{
   "name": "{{page.name}}",
   "text": "{{ page.content | markdownify | newline_to_br | strip_newlines | replace: '<br />', ' ' | strip_html | strip }}"
 }{% unless forloop.last %},{% endunless %}]
