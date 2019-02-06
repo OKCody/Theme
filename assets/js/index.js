f541f5f674e08c0cf6c54265d1abfdc6616764f4
Cody Taylor <codyalantaylor@gmail.com>
2019-02-06 10:41:43 -0600

    remove spaces

--- assets/js/index.js
+++ assets/js/index.js
@@ -5,6 +5,6 @@ layout: null
 {% for page in site.pages %}
 var documents = [{
   "name": "{{page.name}}",
-  "text": "{{ page.content | markdownify | strip_html }}"
+  "text": "{{ page.content | markdownify | strip_html | strip }}"
 }{% unless forloop.last %},{% endunless %}]
 {% endfor %}
