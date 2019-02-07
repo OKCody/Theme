dbcb3fd06571009686869f9abbd8755f3c62c770
Cody Taylor <cody@Codys-Mac-mini.local>
2019-02-06 20:39:21 -0600

    assemble index

--- assets/js/index.js
+++ assets/js/index.js
@@ -2,19 +2,30 @@
 layout: null
 ---
 
-var index = [
-{% for page in site.html_pages %}
-{
-  "name": "{{page.name}}",
-  "text": "{{ page.content | markdownify | newline_to_br | strip_newlines | replace: '<br />', ' ' | strip_html | strip }}"
-}{% unless forloop.last %},{% endunless %}
-{% endfor %}
-]
+function apollo(callback){
+  console.log('1');
+  var index = [
+  {% for page in site.html_pages %}
+  {
+    "name": "{{page.name}}",
+    "text": "{{ page.content | markdownify | newline_to_br | strip_newlines | replace: '<br />', ' ' | strip_html | strip }}"
+  }{% unless forloop.last %},{% endunless %}
+  {% endfor %}
+  ]
+  console.log('2');
+  callback(index)
+}
 
-var idx = lunr(function () {
-  this.ref('id')
-  this.field('body')
-  this.metadataWhitelist = ['position']
+function lander(index){
+  console.log('3');
+  var idx = lunr(function () {
+    this.ref('name')
+    this.field('text')
+    this.metadataWhitelist = ['position']
 
-  index.forEach(function (doc) { this.add(doc) }, this)
-})
+    index.forEach(function (doc) { this.add(doc) }, this)
+  })
+}
+
+console.log('0');
+apollo(lander(index));
