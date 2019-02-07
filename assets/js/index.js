8523e08561ecae1b1b1368ca3e520c13cd43fb26
Cody Taylor <cody@Codys-Mac-mini.local>
2019-02-06 20:54:38 -0600

    assemble index

--- assets/js/index.js
+++ assets/js/index.js
@@ -6,12 +6,13 @@ var idx = lunr(function () {
   this.field('name')
   this.field('text')
 
-  [
+  var index = [
     {% for page in site.html_pages %}
     {
       "name": "{{page.name}}",
       "text": "{{ page.content | markdownify | newline_to_br | strip_newlines | replace: '<br />', ' ' | strip_html | strip }}"
     }{% unless forloop.last %},{% endunless %}
     {% endfor %}
-  ].forEach(function(doc){this.add(doc)}, this);
+  ]
+  index.forEach(function(doc){this.add(doc)}, this);
 });
