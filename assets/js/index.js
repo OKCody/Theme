83b906af19600ee8dd8d45dfc1d847ba6bbf6816
Cody Taylor <cody@Codys-Mac-mini.local>
2019-02-09 16:18:29 -0600

    index title test

--- assets/js/index.js
+++ assets/js/index.js
@@ -4,9 +4,12 @@ layout: null
 
 window.index = [
     {% for page in site.html_pages %}
+    {% assign h2_open_start = page.content|split: '<h2' %}
+    {% assign h2_open_end = h2_open_start[1]| split: '>' %}
+    {% assign h2_content_array = h2_open_end[1]| split: '</h2' %}
     {
       "name": "{{page.name}}",
-      "title": "{% assign h2_open_start = page.content|split: '<h2'%} {% assign h2_open_end = h2_open_start[1]| split: '>'%} {% assign h2_content_array = h2_open_end[1]| split: '</h2'%} {{ h2_content }}",
+      "title": "{{ h2_content }}",
       "text": "{{ page.content | markdownify | newline_to_br | strip_newlines | replace: '<br />', ' ' | strip_html | strip }}"
     }{% unless forloop.last %},{% endunless %}
     {% endfor %}
