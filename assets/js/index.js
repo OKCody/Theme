1df7b38efbea88abd4bf523e9fad695135efb1e1
Cody Taylor <cody@Codys-Mac-mini.local>
2019-02-09 16:51:27 -0600

    index title test

--- assets/js/index.js
+++ assets/js/index.js
@@ -4,13 +4,14 @@ layout: null
 
 window.index = [
     {% for page in site.html_pages %}
-    {% assign h2_open = page.content |  markdownify | split: '<h2' %}
+    {% assign content = page.content | markdownify %}
+    {% assign h2_open = content | split: '<h2' %}
     {% assign h2_end = h2_open[1] | split: '>' %}
     {% assign h2_array = h2_end[1] | split: '</h2' %}
     {
-      "name": "{{page.name}}",
+      "name": "{{ page.name }}",
       "title": "{{ h2_array[0] }}",
-      "text": "{{ page.content | markdownify | newline_to_br | strip_newlines | replace: '<br />', ' ' | strip_html | strip }}"
+      "text": "{{ content | newline_to_br | strip_newlines | replace: '<br />', ' ' | strip_html | strip }}"
     }{% unless forloop.last %},{% endunless %}
     {% endfor %}
   ];
