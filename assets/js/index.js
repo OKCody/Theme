abed6e18975df1724f47ac27259724014d88bbf2
Cody Taylor <codyalantaylor@gmail.com>
2019-02-14 10:02:02 -0600

    titles from headings

--- assets/js/index.js
+++ assets/js/index.js
@@ -4,11 +4,13 @@ layout: null
 
 {% capture search_index %}
 window.index = [
+    {% for page in site.html_pages %}
+    {% assign content = page.content | markdownify %}
     {
       "name": "{{ page.name }}",
       "url": "{{ site.baseurl }}/{{ page.name }}",
-      "title": "{{ page.title | default: page.name }}",
-      "text": "{{ content | replace: 'Preface','' | replace: 'preface', '' | replace: 'Chapter', '' | replace: 'chapter', '' | replace: 'Summary', '' | replace: 'summary', '' | replace: 'Section', '' | replace: 'section', ''}}"
+      "title": "{{ headings[0] | default: page.name }}",
+      "text": "{{ content | newline_to_br | strip_newlines | replace: '<br />', ' ' | strip_html | strip | replace: 'Preface','' | replace: 'preface', '' | replace: 'Chapter', '' | replace: 'chapter', '' | replace: 'Summary', '' | replace: 'summary', '' | replace: 'Section', '' | replace: 'section', ''}}"
     }{% unless forloop.last %},{% endunless %}
     {% endfor %}
   ];
